c6b9aee7987cfa6cdf82c9f3b3343c300336763e
Dinakara Charan Vasa <vasadinakaracharan@gmail.com>
2026-02-11 19:53:24 +0530

    shell

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+PERSON1=CHARAN
+PERSON2=DILEEP
+
+echo "$PERSON1 :: Hello $PERSON2, How are you?"
+echo "$PERSON2 :: Hii $PERSON1, I am fine thanks for asking.. How are you doing?"
+echo "$PERSON1 :: Doing fine. what about you learning $PERSON2?"
+echo "$PERSON2 :: I am learning shell. what about you?"
