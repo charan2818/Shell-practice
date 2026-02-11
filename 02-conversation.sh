de2a21fa2efc0c9dfbbf9dc959518a179bb2e5c4
Dinakara Charan Vasa <vasadinakaracharan@gmail.com>
2026-02-11 19:52:36 +0530

    shell

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
  
- PERSON1=CHARAN
- PERSON2=DILEEP
-
-echo "$PERSON1 :: Hello $PERSON2, How are you?"
-echo "$PERSON2 :: Hii $PERSON1, I am fine thanks for asking.. How are you doing?"
-echo "$PERSON1 :: Doing fine. what about you learning $PERSON2?"
-echo "$PERSON2 :: I am learning shell. what about you?"
+echo "Suresh :: Hello Ramesh, How are you?"
+echo "Ramesh :: Hii Suresh, I am fine thanks for asking.. How are you doing?"
+echo "Suresh :: Doing fine. what about you learning Ramesh?"
+echo "Ramesh :: I am learning shell. what about you?"
