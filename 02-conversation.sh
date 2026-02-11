b2747c5a3bd1a760fef890e34cfe33e1abbd4cc9
Dinakara Charan Vasa <vasadinakaracharan@gmail.com>
2026-02-11 19:13:52 +0530

    Shell

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh :: Hello Ramesh, How are you?"
+echo "Ramesh :: Hii Suresh, I am fine thanks for asking.. How are you doing?"
+echo "Suresh :: Doing fine. what about you learning Ramesh?"
+echo "Ramesh :: I am learning shell. what about you?"
