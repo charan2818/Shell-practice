bcaa4802c01c6bb05198626aecfa3eda8005000d
Dinakara Charan Vasa <vasadinakaracharan@gmail.com>
2026-02-16 19:07:07 +0530

    Data types

---
+++ 08-datatypes.sh
@@ -0,0 +1,15 @@
+
+NUM1= 100
+NUM2= 200
+
+echo= $((NUM1+NUM2))
+
+
+echo "sum is: $SUM"
+
+FRUITS=("Apple" "Banana" "Pomo")
+
+echo "Fruits are: ${FRUITS[@]}" 
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
