8261269e172eaa4b8e45fcfce310cb12de5ac5c2
Dinakara Charan Vasa <vasadinakaracharan@gmail.com>
2026-02-16 20:11:01 +0530

    Data types

---
+++ 09-conditions.sh
@@ -0,0 +1,13 @@
+NUMBER=25
+
+if [ $NUMBER > 20]; then
+    echo "Given number is larger than 20"
+
+elif [ $NUMBER = 20]; then
+    echo "Given number is equal to 20"
+
+else [ $NUMBER < 20]; then
+    echo "Given number is less than 20"
+
+fi
+
