1f378b1d491183b22222516f86e3befd98302597
Dinakara Charan Vasa <vasadinakaracharan@gmail.com>
2026-02-19 11:28:22 +0530

    Shell

---
+++ 11-functions.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "please run this script with root user access"
+    exit 1
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+
+}
+
+dnf install nginx -y
+VALIDATE "Installing Nginx"
+
+dnf install mysql -y
+VALLIDATE "Installing MySQL"
+
+dnf install nodejs -y
+VALIDATE "Installing Nodejs"
