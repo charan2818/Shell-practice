7d19739ee8daee428c31214ec2782e486ac432c0
Dinakara Charan Vasa <vasadinakaracharan@gmail.com>
2026-06-22 09:20:02 +0530

    commit

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
- 
-PERSON1=CHARAN
-PERSON2=DILEEP
 
-echo "$PERSON1 :: Hello $PERSON2, How are you?"
-echo "$PERSON2 :: Hii $PERSON1, I am fine thanks for asking.. How are you doing?"
-echo "$PERSON1 :: Doing fine. what about you learning $PERSON2?"
-echo "$PERSON2 :: I am learning shell. what about you?"
+PERSON1=CHARAN
+PERSON2=RAMA
+
+echo " $PERSON1 :: Hello $PERSON2, How are you ? "
+echo " $PERSON2 :: Hii $PERSON1, Thanks for asking i am good how are you doing..? "
+echo " $PERSON1 :: Doing good. What your son is doing..? "
+echo " $PERSON2 :: Yeah he is learning Devsecops "
+echo " $PERSON1 :: Hoo that's great  " 
+echo " $PERSON2 :: Ok $PERSON1 see you later i have some work.. "
+echo " $PERSON1 :: Ok $PERSON2 gud to see you again.... Byee "
+echo " $PERSON2 :: Byee... " 
