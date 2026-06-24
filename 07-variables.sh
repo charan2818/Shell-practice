cf7ca3c82f2a23b3ac40c35d904a8fe210e0e47c
Dinakara Charan Vasa <vasadinakaracharan@gmail.com>
2026-06-24 10:57:05 +0530

    re-opened

--- 07-variables.sh
+++ 07-variables.sh
@@ -10,4 +10,3 @@ echo "Home Directory of current user: $HOME"
 echo "PID of this script: $$"
 sleep 100 &
 echo "PID of recently executed background process: $!"
-echo "All args passed to script: $*"
